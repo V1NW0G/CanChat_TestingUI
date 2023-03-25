de55def108613562cdb2d0a2d6a4eed5ef53d303
V1NW0G <vincentwongck110@gmail.com>
2023-03-25 19:07:48 +0800

    imporved front end

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,161 +1,102 @@
-import {View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { Configuration, OpenAIApi } from 'openai';
-import Constants from 'expo-constants'
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, ScrollView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Chat = () => {
-    const [data, setData] = useState([{"type": "user", "text": "What is your name"}, {"type": "bot","text": "My name is Sarah."}]);
-    const apiKey = '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' 
-    const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions'
-    // const apiKey = Constants.manifest.extra.openaiKey
-    // const apiUrl = Constants.manifest.extra.openaiUrl
-    const [textInput , setTextInput ] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const scrollViewRef = useRef();
 
-    // const handleSend = async (textInput) => {
-    //     const url = "https://api.openai.com/v1/chat/completions";
-    //     const payload = {
-    //         model: "gpt-3.5-turbo",
-    //         messages: [
-    //         { role: "system", content: "You are a helpful assistant." },
-    //         { role: "user", content: textInput },
-    //         ],
-    //         temperature: 0.4,
-    //         max_tokens: 50,
-    //     };
+  const handleSend = () => {
+    setMessages([...messages, { type: 'user', text: inputText }]);
+    setInputText('');
+  };
 
-    //     const options = {
-    //         contentType: "application/json",
-    //         headers: { Authorization: "Bearer " + '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'},
-    //         payload: JSON.stringify(payload),
-    //       };
+  useEffect(() => {
+    scrollViewRef.current.scrollToEnd();
+  }, [messages]);
 
-    //     const text = JSON.parse(UrlFetchApp.fetch(url, options).getContentText());
-    //     return text.choices[0].message.content.trim();
-
-    //     console.log(textInput)
-    //     console.log(text)
-    // };
-      
-    const handleSend = async () => {
-        // try {
-        if (textInput !== ''){
-          const prompt = textInput;
-          const response = await axios.post(apiUrl, {
-            prompt: prompt,
-            max_tokens: 10,
-            temperature: 0.5,
-          }, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${apiKey}`
-            }
-          });
-          const text = response.data.choices[0].text;
-          setData([...data, {type: 'user', 'text':textInput},{ type: 'bot', 'text':text}]);
-          console.log(data)
-          console.log(text)
-
-          setTextInput('');
-        }
-        else { 
-            Alert.alert("Empty input");
-        }
-    } 
-        // catch (error) {
-        //   console.error(error);
-        // }
-    // }
-      
-
-    return (
-        <View style={styles.container}>
-            <Text>Fuck</Text>
-            <View style={styles.list}>
-                <FlatList 
-                    data = {data}
-                    keyExtractor = {(item, index) => index.toString()}
-                    style = {styles.body}
-                    renderItem = {({item}) => (
-                        <View style = {{flexDirection:'row', padding:10}}>
-                            <Text style = {{fontWeight:'bold',color: item.type === 'user' ? 'green' : 'red'}}>{item.type === 'user' ? 'DickHead: ' : 'Bot: '}</Text>
-                            <Text style = {styles.bot}> {item.text} </Text>
-                        </View>
-                    )}
-                    contentContainerStyle={{
-                        flexGrow: 1,
-                        }}
-                />
-            </View>
-                <TextInput 
-                    style = {styles.input}
-                    value = {textInput}
-                    onChangeText = {text => setTextInput(text)}
-                    placeholder = "  Ask me anything"
- 
-                />
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress = {handleSend}
-
-                >
-                    <Text style={styles.buttonText}>send</Text>
-                </TouchableOpacity>
-
-        </View>
-    )
-}
-
-export default Chat
+  return (
+    <View style={styles.container}>
+      <ScrollView ref={scrollViewRef} style={styles.chatContainer}>
+        {messages.map((item, index) => (
+          <View key={index} style={item.type === 'user' ? styles.userBubble : styles.botBubble}>
+            <Text style={styles.messageText}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={inputText}
+          onChangeText={setInputText}
+          placeholder="Ask me anything"
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: "#fff",
-    },
-
-    body:{
-        backgroundColor: "white",
-        width: '102%',
-        margin: 10
-    },
-
-    bot:{
-        fontSize: 16
-    },
-    
-    list:{
-        flex:1,
-        width: '100%'
-    },
-
-    input:{
-        borderWidth: 1,
-        borderColor : 'black',
-        width : '90%',
-        height : 60,
-        marginBottom: 10,
-        borderRadius: 10       
-    },
-
-    button:{
-        backgroundColor: '#fff',
-        width: '80%',
-        height: 60, 
-        borderRadius: 10,   
-        justifyContent: 'center',
-        alignItems: 'center', 
-        marginBottom: 10
-    },
-
-    buttonText:{
-        fontSize: 25,
-        fontWeight: 'bold',
-        color: 'blue'
-    }
-
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  chatContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    marginRight: 10,
+  },
+  sendButton: {
+    backgroundColor: 'blue',
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  userBubble: {
+    backgroundColor: 'green',
+    alignSelf: 'flex-end',
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    maxWidth: '70%',
+  },
+  botBubble: {
+    backgroundColor: 'lightgray',
+    alignSelf: 'flex-start',
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    maxWidth: '70%',
+  },
+  messageText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
+export default Chat;
